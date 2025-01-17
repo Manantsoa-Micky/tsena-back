d18fbf04cb0c682dffaf82a368146fc36cd4001e
Manantsoa-Micky <mickymickymarc@gmail.com>
2025-01-17 13:15:49 +0300

    logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,8 +1,19 @@
-import { createLogger, transports, format } from 'winston';
+import { createLogger, transports, format, error, warn } from 'winston';
 
+const customFormat = format.printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] [${level.toUpperCase()}]: ${message}`;
+});
 const logger = createLogger({
   level: 'info',
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.cli({
+    colors: {
+      info: 'green',
+      error: 'red',
+      warn: 'yellow',
+    },
+    all: true,
+  }),
+
   transports: [new transports.Console()],
 });
 
