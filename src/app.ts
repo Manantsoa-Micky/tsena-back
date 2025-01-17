f07315257b3f8efd88645955f83e810492944c86
Manantsoa-Micky <mickymickymarc@gmail.com>
2025-01-17 13:16:32 +0300

    refactor main and server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { setupSwagger } from './common/swagger';
+import errorHandler from './common/errorHandler';
+import userRoutes from './modules/user/user.routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use(errorHandler);
+setupSwagger(app);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import { Server } from 'http';
+import { connectDB } from './common/mongoConnection';
+import { logger } from './common/logger';
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    server = app.listen(PORT, () => {
+      logger.info(`⚡ Server running on port: ${PORT}`);
+    });
+  } catch (error) {
+    logger.error('Failed to connect to the database', error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(0);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: any) => {
+  logger.error(error.message);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
