5a4a3623eecfc1eeef5827edbd3c97fd74bef485
Manantsoa-Micky <mickymickymarc@gmail.com>
2025-01-17 13:15:40 +0300

    error handler

--- src/common/errorHandler.ts
+++ src/common/errorHandler.ts
@@ -1,6 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal Server error' });
-}
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+  const errors = err.errors || [];
+
+  res.status(status).json({
+    status,
+    message,
+    errors,
+  });
+};
+
+export default errorHandler;
